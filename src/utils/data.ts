9cb187ee601f17ae96b53c9e602ca12ddf94359e
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-12-22 16:58:10 -0300

    chore: added new projects and fixed links

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,70 +1,66 @@
+import astro from "../assets/icons/astro_dark.svg";
 import css from "../assets/icons/css.svg";
+import javascript from "../assets/icons/javascript_1.svg";
+import react from "../assets/icons/react.svg";
+import sass from "../assets/icons/sass.svg";
+import animais from "../assets/images/animais.png";
 import bikcraft from "../assets/images/bikcraft.png";
-
-type Props = {
-  img: ImageMetadata;
-  title: string;
-  description: string;
-  href: string;
-  alt: string;
-  tec1: ImageMetadata;
-};
+import disney from "../assets/images/disney.png";
+import dogs from "../assets/images/Dogs.png";
+import eplay from "../assets/images/eplay.png";
+import zingen from "../assets/images/zingen.png";
 
 export const PROJECTS = [
   {
-    img: bikcraft,
-    title: "BikCraft",
-    description: "Site de Bicicletas Elétricas",
-    href: "bikcraft",
-    alt: "projeto Tech News",
-    tec1: css,
+    img: eplay,
+    title: "Eplay",
+    description:
+      "Eplay é uma plataforma de venda de games on-line com os melhores games da atualidade",
+    href: "eplay",
+    alt: "projeto Dogs desenvolvido por Emmanuel Oliveira",
+    tec1: react,
   },
   {
     img: bikcraft,
     title: "BikCraft",
     description: "Site de Bicicletas Elétricas",
     href: "bikcraft",
-    alt: "projeto Tech News",
+    alt: "projeto Bikcraft Desenvolvido por Emmanuel Oliveira",
     tec1: css,
   },
   {
-    img: bikcraft,
-    title: "BikCraft",
-    description: "Site de Bicicletas Elétricas",
-    href: "bikcraft",
-    alt: "projeto Tech News",
-    tec1: css,
+    img: dogs,
+    title: "Dogs",
+    description: "Dogs é uma rede social de cachorros",
+    href: "dogs",
+    alt: "projeto Dogs desenvolvido por Emmanuel Oliveira",
+    tec1: react,
   },
   {
-    img: bikcraft,
-    title: "BikCraft",
-    description: "Site de Bicicletas Elétricas",
-    href: "bikcraft",
-    alt: "projeto Tech News",
-    tec1: css,
+    img: zingen,
+    title: "Zingen",
+    description:
+      "É um aplicativo de música, encontre suas musicas favoritas aqui",
+    href: "zingen",
+    alt: "projeto zingen desenvolvido por Emmanuel Oliveira",
+    tec1: astro,
+  },
+
+  {
+    img: disney,
+    title: "Clone do Disney+",
+    description: "Venha conhecer o clone do Aplicativo Disney+",
+    href: "disneyplus",
+    alt: "projeto clone do Disney desenvolvido por Emmanuel Oliveira",
+    tec1: sass,
   },
   {
-    img: bikcraft,
-    title: "BikCraft",
-    description: "Site de Bicicletas Elétricas",
-    href: "bikcraft",
-    alt: "projeto Tech News",
-    tec1: css,
-  },
-  {
-    img: bikcraft,
-    title: "BikCraft",
-    description: "Site de Bicicletas Elétricas",
-    href: "bikcraft",
-    alt: "projeto Tech News",
-    tec1: css,
-  },
-  {
-    img: bikcraft,
-    title: "BikCraft",
-    description: "Site de Bicicletas Elétricas",
-    href: "bikcraft",
-    alt: "projeto Tech News",
-    tec1: css,
+    img: animais,
+    title: "Animais Fantásticos",
+    description:
+      "Venha conhecer essa Landing page fantástica com as curiosidades dos animais do nosso planeta",
+    href: "/animaisfantasticos",
+    alt: "projeto Animais Fantásticos desenvolvido por Emmanuel Oliveira",
+    tec1: javascript,
   },
 ];
